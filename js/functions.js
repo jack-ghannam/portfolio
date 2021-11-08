5f4daab2f87b3bddb8c8fc121b45573b6f5e5f97
jack-ghannam <93916638+jack-ghannam@users.noreply.github.com>
2021-11-08 06:47:31 -0500

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -145,7 +145,7 @@ function newPopup(url) {
 
 $('#example').simpleBarGraph({
     data: [
-        { key: 'Citations in 2021', value: 161 },
+        { key: 'Citations in 2021', value: 173 },
         { key: 'Citations in 2020', value: 49 },
         { key: 'Citations in 2019', value: 9 },
         { key: 'Citations in 2018', value: 1 },
